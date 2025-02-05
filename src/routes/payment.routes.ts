67be4bead721b384d73ffe10bc8c0a2eaea24131
ypreiser <ypreiser@gmail.com>
2025-02-05 13:01:43 +0200

    yaad-callback

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,10 +1,20 @@
-import { Router } from 'express';
-import { PaymentController } from '../controllers/payment.controller.js';
+import { Router } from "express";
+import { PaymentController } from "../controllers/payment.controller.js";
 
 const router = Router();
 const paymentController = new PaymentController();
 
-router.post('/payment', (req, res) => paymentController.processPayment(req, res));
-router.get('/yaad-callback', (req, res) => paymentController.handleYaadWebhook(req, res));
+router.post("/payment", (req, res) =>
+  paymentController.processPayment(req, res)
+);
+router.get("/yaad-callback", (req, res) =>
+  paymentController.handleYaadWebhook(req, res)
+);
+router.get("/payment-success", (req, res) =>
+  paymentController.showSuccessPage(req, res)
+);
+router.get("/payment-cancelled", (req, res) =>
+  paymentController.showCancelPage(req, res)
+);
 
-export default router; 
+export default router;
